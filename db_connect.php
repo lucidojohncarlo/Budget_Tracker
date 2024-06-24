20ee8a6f04c386aab9980726fe036c7812d9c624
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-24 20:36:00 +0800

    24-06-24

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $conn = mysqli_init();
 
 // Set the SSL parameters
-$ca_cert = "/site/wwwroot/certs/DigiCertGlobalRootCA.crt.pem"; // Path to the CA certificate
+$ca_cert = "/site/wwwroot/certs/DigiCertGlobalRootCA.crt.pem"; // Path to the CA certificate on Azure
 mysqli_ssl_set($conn, NULL, NULL, $ca_cert, NULL, NULL);
 
 $servername = "lucidojohncarlo.mysql.database.azure.com";
@@ -12,7 +12,7 @@ $dbname = "budget_tracker";
 $port = 3306;
 
 if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error() . " (Error number: " . mysqli_connect_errno() . ")");
 }
 
 echo "Connected successfully using SSL";
