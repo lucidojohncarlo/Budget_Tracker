f5b7bbe98496bc9f85b61da83b8af4a1a8d2c40a
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-24 19:47:03 +0800

    24-06-24

--- db_connect.php
+++ db_connect.php
@@ -5,8 +5,8 @@ $conn = mysqli_init();
 $ca_cert = "DigiCertGlobalRootCA.crt.pem"; // Path to the CA certificate
 mysqli_ssl_set($conn, NULL, NULL, $ca_cert, NULL, NULL);
 
-$servername = "johncarlolucido.mysql.database.azure.com";
-$username = "johncarlo";
+$servername = "lucidojohncarlo.mysql.database.azure.com";
+$username = "lucidojohncarlo";
 $password = "Jhared123";
 $dbname = "budget_tracker";
 $port = 3306;
@@ -16,26 +16,6 @@ if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, $por
 }
 
 echo "Connected successfully using SSL";
-
-//$servername ="johncarlolucido.mysql.database.azure.com" ;
-//$username = "johncarlo";
-//$password = "Jhared123";
-//$dbname = "budget_tracker";
-//$ssl_ca = "C:\xampp\htdocs\Budget_Tracker\DigiCertGlobalRootCA.pem";
-//
-//
-//// Create a new MySQLi object
-//$conn = new mysqli();
-//
-//// Set the SSL CA certificate
-//$conn->ssl_set(NULL, NULL, $ssl_ca, NULL, NULL);
-//
-//// Connect to the database
-//$conn->real_connect($servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
-//
-//// Check connection
-//if ($conn->connect_error) {
-//    die("Connection failed: " . $conn->connect_error);
 
 //
 // $servername ="localhost" ;
